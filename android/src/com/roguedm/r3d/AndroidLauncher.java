aed96b5a3736b51dbfb104f0fded27e404096cc9
Jim <jim.jjones@gmail.com>
2024-01-04 19:21:06 -0800

    Initial Update

---
+++ android/src/com/roguedm/r3d/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.roguedm.r3d;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.roguedm.r3d.Rogue3d;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Rogue3d(), config);
+	}
+}
---
+++ desktop/src/com/roguedm/r3d/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.roguedm.r3d;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.roguedm.r3d.Rogue3d;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Rogue3d");
+		new Lwjgl3Application(new Rogue3d(), config);
+	}
+}
