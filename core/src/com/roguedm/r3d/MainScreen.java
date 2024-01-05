ace438297f669803bd0e35847a474925b0f092d9
Jim <jim.jjones@gmail.com>
2024-01-04 19:56:55 -0800

    Initial Update

---
+++ core/src/com/roguedm/r3d/MainScreen.java
@@ -0,0 +1,59 @@
+package com.roguedm.r3d;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainScreen implements Screen {
+
+    private Game parent;
+
+    private SpriteBatch batch;
+
+    public MainScreen(Game parent) {
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
+        batch.begin();
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        if (batch != null) {
+            batch.dispose();
+            batch = null;
+        }
+    }
+
+}
--- core/src/com/roguedm/r3d/Rogue3d.java
+++ core/src/com/roguedm/r3d/Rogue3d.java
@@ -1,31 +1,23 @@
 package com.roguedm.r3d;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Rogue3d extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Rogue3d extends Game {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new MainScreen(this));
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		if (this.getScreen() != null) {
+			this.getScreen().dispose();
+		}
 	}
+
 }
